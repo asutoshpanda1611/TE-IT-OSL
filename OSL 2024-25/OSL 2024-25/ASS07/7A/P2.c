6b11f94e4dc0ae90badc624cfa7a7dd8af1e6ff1
Asutosh panda <122630330+asutoshpanda1611@users.noreply.github.com>
2024-10-22 09:19:49 +0530

    Add files via upload

---
+++ OSL 2024-25/OSL 2024-25/ASS07/7A/P2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+#include<fcntl.h>
+#include<unistd.h>
+#define FIFO_PIPE "xyz"
+int main()
+{
+int fd, i, c=0, w=0, l=0;
+char str[100];
+fd=open(FIFO_PIPE,O_RDONLY);	    // open the pipe
+read(fd, str, sizeof(str));		    // read from pipe
+for(i=0; str[i]!='\0'; i++)		
+{
+	if(str[i] == ' ' || str[i] == '\n') 		w++;
+	if(str[i] == '\n')							l++;
+												c++;
+}
+printf("\n No of characters = %d", c);	
+printf("\n No of lines = %d", l+1);
+printf("\n No of words = %d \n", w);
+close(fd);
+return 0;
+}
