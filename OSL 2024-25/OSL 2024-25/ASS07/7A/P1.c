5ebe980585f6ef822bacbfcb6dcb9d01aef1b47c
Asutosh panda <122630330+asutoshpanda1611@users.noreply.github.com>
2024-11-09 03:25:23 +0530

    Update P1.c

--- OSL 2024-25/OSL 2024-25/ASS07/7A/P1.c
+++ OSL 2024-25/OSL 2024-25/ASS07/7A/P1.c
@@ -3,20 +3,22 @@
 #include<fcntl.h>
 #include<unistd.h>
 #define FIFO_PIPE "xyz"
-int main()
-{
-	int fd, i=0;
-	char str[200],ch;
-	mkfifo(FIFO_PIPE, 0644);    // Pipe created	
-	printf("\n Pipe Successfully created \n");
-	printf("\n Enter Your message (press @ to terminate) \n");
-	while((ch=fgetc(stdin)) != '@')
-	{
-		str[i++] = ch;
-	}
-	str[i]='\0'; // Null character at the end 
-	fd=open(FIFO_PIPE,O_WRONLY); // Open the pipe
-	write(fd, str, strlen(str)); // write into the pipe
-	close(fd);
-	return 0;
+
+int main() {
+    int fd, i = 0;
+    char str[200], ch;
+
+    mkfifo(FIFO_PIPE, 0644);  
+    printf("\n Pipe Successfully created \n");
+    
+    printf("\n Enter Your message (press @ to terminate) \n");
+    while((ch = fgetc(stdin)) != '@') {  
+        str[i++] = ch;  
+    }
+    str[i] = '\0';  
+
+    fd = open(FIFO_PIPE, O_WRONLY);  
+    write(fd, str, strlen(str));     
+    close(fd);  // Close the pipe
+    return 0;
 }
