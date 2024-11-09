2308012e0d2dbec4b223fc92ec8710a354ed8d70
Asutosh panda <122630330+asutoshpanda1611@users.noreply.github.com>
2024-11-09 14:08:49 +0530

    Update Banker.c

--- OSL 2024-25/OSL 2024-25/ASS05/Banker.c
+++ OSL 2024-25/OSL 2024-25/ASS05/Banker.c
@@ -1,74 +1,70 @@
 #include <stdio.h>
-int main()
-{
-int Max[10][10],need[10][10],alloc[10][10],avail[10],completed[10],safeSequence[10];
-int p, r, i, j, process, count=0;
 
-printf("Enter the no of processes : ");         scanf("%d", &p);
-for(i = 0; i< p; i++)   completed[i] = 0;
-printf("\nEnter the no of resources : ");       scanf("%d", &r);
+int main() {
+    int Max[10][10], need[10][10], alloc[10][10], avail[10], completed[10], safeSequence[10];
+    int p, r, i, j, process, count = 0;
 
-printf("\nEnter the Max/Claim Matrix for each process : ");
-for(i = 0; i < p; i++)
-{
-	printf("\nFor process %d : ", i + 1);
-	for(j = 0; j < r; j++)  		scanf("%d", &Max[i][j]);
-}
+    printf("Enter the no of processes : ");
+    scanf("%d", &p);
+    for (i = 0; i < p; i++) completed[i] = 0;
 
-printf("\nEnter the allocation for each process : ");
-for(i = 0; i < p; i++)
-{
-	printf("\nFor process %d : ",i + 1);    
-	for(j = 0; j < r; j++)  		scanf("%d", &alloc[i][j]);	
-}
+    printf("\nEnter the no of resources : ");
+    scanf("%d", &r);
 
-printf("\nEnter the Available Resources : ");
-for(i = 0; i < r; i++)      		scanf("%d", &avail[i]);	
+    printf("\nEnter the Max/Claim Matrix for each process : ");
+    for (i = 0; i < p; i++) {
+        printf("\nFor process %d : ", i + 1);
+        for (j = 0; j < r; j++) scanf("%d", &Max[i][j]);
+    }
 
-	for(i = 0; i < p; i++)
-		for(j = 0; j < r; j++)
-			need[i][j] = Max[i][j] - alloc[i][j];
-do
-{
-	process = -1;
-	for(i = 0; i < p; i++)
-	{
-		if(completed[i] == 0)
-		{
-			process = i ;
-			for(j = 0; j < r; j++)
-			{
-				if(avail[j] < need[i][j])
-				{
-					process = -1;
-					break;
-				}
-			}
-		}
-		if(process != -1) 			break;
-	}
+    printf("\nEnter the allocation for each process : ");
+    for (i = 0; i < p; i++) {
+        printf("\nFor process %d : ", i + 1);
+        for (j = 0; j < r; j++) scanf("%d", &alloc[i][j]);
+    }
 
-	if(process != -1)
-	{
-		printf("\nProcess %d runs to completion!", process + 1);
-		safeSequence[count] = process + 1;
-		count++;
-		for(j = 0; j < r; j++)
-		{
-			avail[j] += alloc[process][j];
-			alloc[process][j] = 0;
-			Max[process][j] = 0;
-			completed[process] = 1;
-		}
-	}
-}while(count != p && process != -1);
+    printf("\nEnter the Available Resources : ");
+    for (i = 0; i < r; i++) scanf("%d", &avail[i]);
 
-if(count == p)
-{
-	printf("\nThe system is in a safe state!!\n");
-	printf("Safe Sequence is  ");
-	for( i = 0; i < p; i++) 		printf("%d  ", safeSequence[i]);
-}
-else
-	printf("\nThe system is in an unsafe state!!");
+    for (i = 0; i < p; i++)
+        for (j = 0; j < r; j++)
+            need[i][j] = Max[i][j] - alloc[i][j];
+
+    do {
+        process = -1;
+        for (i = 0; i < p; i++) {
+            if (completed[i] == 0) {
+                process = i;
+                for (j = 0; j < r; j++) {
+                    if (avail[j] < need[i][j]) {
+                        process = -1;
+                        break;
+                    }
+                }
+            }
+            if (process != -1) break;
+        }
+
+        if (process != -1) {
+            printf("\nProcess %d runs to completion!", process + 1);
+            safeSequence[count] = process + 1;
+            count++;
+            for (j = 0; j < r; j++) {
+                avail[j] += alloc[process][j];
+                alloc[process][j] = 0;
+                Max[process][j] = 0;
+                completed[process] = 1;
+            }
+        }
+    } while (count != p && process != -1);
+
+    if (count == p) {
+        printf("\nThe system is in a safe state!!\n");
+        printf("Safe Sequence is  ");
+        for (i = 0; i < p; i++) printf("%d  ", safeSequence[i]);
+    } else {
+        printf("\nThe system is in an unsafe state!!");
+    }
+
+    return 0;
 }
