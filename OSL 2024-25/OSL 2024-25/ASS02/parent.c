81f0c08d4ba975c23b4f9679261a06e430c84fd1
Asutosh panda <122630330+asutoshpanda1611@users.noreply.github.com>
2024-11-09 23:14:08 +0530

    Update parent.c

--- OSL 2024-25/OSL 2024-25/ASS02/parent.c
+++ OSL 2024-25/OSL 2024-25/ASS02/parent.c
@@ -3,61 +3,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void merge(int arr[], int l, int m, int r)
-{
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 =  r - m;
-    int L[n1], R[n2];
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1+ j];
- 
-   
-    i = 0; 
-    j = 0;
-    k = l; 
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= R[j])
-        {
-            arr[k] = L[i];
-            i++;
-        }
-        else
-        {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-    while (i < n1)
-    {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-    while (j < n2)
-    {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void mergeSort(int arr[], int l, int r)
-{
-    if (l < r)
-    {
-
-        int m = l+(r-l)/2;
-        mergeSort(arr, l, m);
-        mergeSort(arr, m+1, r);
-		merge(arr, l, m, r);
+// Bubble Sort Function
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
     }
 }
-
 void displayArray(int arr[], int size){
 	for(int i = 0 ; i < size ; i++){
 		printf("%d\t", arr[i]);
@@ -80,7 +41,7 @@ int main(){
 		scanf("%d", &arr[i]);
 	}   
 
-	mergeSort(arr, 0, size-1);
+	bubbleSort(arr, 0, size-1);
 
     for(int i = 0 ; i < size ; i++)
     {
